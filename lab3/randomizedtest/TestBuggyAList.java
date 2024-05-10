b84aa35aa861ef2aa9c56d47390602189ded2ceb
pippen <pippen1108@gmail.com>
2024-05-11 00:03:39 +0800

    lab3 Simple Comparison Test

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -2,11 +2,28 @@ package randomizedtest;
 
 import edu.princeton.cs.algs4.StdRandom;
 import org.junit.Test;
+import timingtest.AList;
+
+import java.util.function.Predicate;
 import static org.junit.Assert.*;
 
 /**
  * Created by hug.
  */
 public class TestBuggyAList {
-  // YOUR TESTS HERE
+    @Test
+    public void testThreeAddThreeRemove() {
+        AListNoResizing<Integer> Alist = new AListNoResizing<>();
+        BuggyAList<Integer> Blist = new BuggyAList<>();
+        for (int i = 0; i < 3; i++){
+            Alist.addLast(i + 4);
+            Blist.addLast(i + 4);
+        }
+        assertEquals(Alist.size(), Blist.size());
+        assertEquals(Alist.removeLast(), Blist.removeLast());
+        assertEquals(Alist.removeLast(), Blist.removeLast());
+        assertEquals(Alist.removeLast(), Blist.removeLast());
+    }
+
+
 }
