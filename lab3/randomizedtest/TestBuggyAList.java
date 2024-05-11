5ca2c5ac4b078fa87a7acbedc9dae8519185dadd
pippen <pippen1108@gmail.com>
2024-05-11 22:55:23 +0800

    lab3

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -15,7 +15,7 @@ public class TestBuggyAList {
     public void testThreeAddThreeRemove() {
         AListNoResizing<Integer> Alist = new AListNoResizing<>();
         BuggyAList<Integer> Blist = new BuggyAList<>();
-        for (int i = 0; i < 3; i++){
+        for (int i = 0; i < 3; i++) {
             Alist.addLast(i + 4);
             Blist.addLast(i + 4);
         }
@@ -25,5 +25,33 @@ public class TestBuggyAList {
         assertEquals(Alist.removeLast(), Blist.removeLast());
     }
 
-
+    @Test
+    public void randomizedTest() {
+        AListNoResizing<Integer> AL = new AListNoResizing<>();
+        BuggyAList<Integer> BL = new BuggyAList<>();
+        int N = 5000;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                AL.addLast(randVal);
+                BL.addLast(randVal);
+            } else if (operationNumber == 1) {
+                // size
+                int alsize = AL.size();
+                int blsizt = BL.size();
+            } else if (operationNumber == 2) {
+                if (AL.size() > 0) {
+                    int algetLast = AL.getLast();
+                    int blgetLast = BL.getLast();
+                }
+            } else if (operationNumber == 3) {
+                if (AL.size() > 0) {
+                    int alremoveLast = AL.removeLast();
+                    int blremoveLast = BL.removeLast();
+                }
+            }
+        }
+    }
 }
