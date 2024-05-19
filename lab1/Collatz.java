8d1278ee0f4f7136561cc47f868f496ecb707ed8
pippen <pippen1108@gmail.com>
2024-05-19 20:01:54 +0800

    lab1

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,13 +1,13 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+ *  @author Pippen Chen
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** return the number of collatz */
     public static int nextNumber(int n) {
-        if (n  == 128) {
+        if (n  == 1) {
             return 1;
-        } else if (n == 5) {
+        } else if (n % 2 == 1) {
             return 3 * n + 1;
         } else {
             return n * 2;
