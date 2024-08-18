e34ca9576866ea5b28ce72d6763f3dcf66b134f7
pippen <pippen1108@gmail.com>
2024-08-18 14:05:26 +0800

    compile error

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -13,91 +13,94 @@ public class Main {
     /** Usage: java gitlet.Main ARGS, where ARGS contains
      *  <COMMAND> <OPERAND1> <OPERAND2> ... 
      */
-    public static void main(String[] args) throws IOException {
-
-        if (args.length == 0) {
-            throw new GitletException("Please enter a command.");
-        }
-        String firstArg = args[0];
-        String text;
-        switch (firstArg) {
-            case "init":
-                Repository.initialCommit();
-                break;
-            case "add":
-                validateRepo();
-                validateNumArgs("add", args, 2);
-                text = args[1];
-                Repository.add(text);
-                break;
-            case "commit":
-                validateRepo();
-                validateNumArgs("commit", args, 2);
-                text = args[1];
-                Repository.commit(text);
-                break;
-            case "log":
-                validateRepo();
-                validateNumArgs("log", args, 1);
-                Repository.log();
-                break;
-            case "global-log":
-                validateNumArgs("global-log", args, 1);
-                Repository.globalLog();
-                break;
-            case "status":
-                validateRepo();
-                validateNumArgs("status", args, 1);
-                Repository.status();
-                break;
-            case "checkout":
-                validateRepo();
-                if (args.length == 3 && args[1].equals("--")) {
-                    String fileName = args[2];
-                    Repository.checkout(fileName);
-                } else if (args.length == 4 && args[2].equals("--")) {
-                    String commitId = args[1];
-                    String fileName = args[3];
-                    Repository.checkout(fileName, commitId);
-                } else if (args.length == 2) {
-                    String branch = args[1];
-                    Repository.checkoutBranch(branch);
-                } else {
-                    throw new GitletException("Incorrect operands.");
-                }
-                break;
-            case "find":
-                validateRepo();
-                validateNumArgs("find", args, 2);
-                text = args[1];
-                Repository.find(text);
-                break;
-            case "reset":
-                validateRepo();
-                validateNumArgs("reset", args, 2);
-                text = args[1];
-                Repository.reset(text);
-                break;
-            case "branch":
-                validateRepo();
-                validateNumArgs("branch", args, 2);
-                text = args[1];
-                Repository.branch(text);
-                break;
-            case "rm-branch":
-                validateRepo();
-                validateNumArgs("rm-branch", args, 2);
-                text = args[1];
-                Repository.rmBranch(text);
-                break;
-            case "rm":
-                validateRepo();
-                validateNumArgs("rm", args, 2);
-                text = args[1];
-                Repository.rm(text);
-                break;
-            default:
-                throw new GitletException("No command with that name exists.");
+    public static void main(String[] args) {
+        try {
+            if (args.length == 0) {
+                throw new GitletException("Please enter a command.");
+            }
+            String firstArg = args[0];
+            String text;
+            switch (firstArg) {
+                case "init":
+                    Repository.initialCommit();
+                    break;
+                case "add":
+                    validateRepo();
+                    validateNumArgs("add", args, 2);
+                    text = args[1];
+                    Repository.add(text);
+                    break;
+                case "commit":
+                    validateRepo();
+                    validateNumArgs("commit", args, 2);
+                    text = args[1];
+                    Repository.commit(text);
+                    break;
+                case "log":
+                    validateRepo();
+                    validateNumArgs("log", args, 1);
+                    Repository.log();
+                    break;
+                case "global-log":
+                    validateNumArgs("global-log", args, 1);
+                    Repository.globalLog();
+                    break;
+                case "status":
+                    validateRepo();
+                    validateNumArgs("status", args, 1);
+                    Repository.status();
+                    break;
+                case "checkout":
+                    validateRepo();
+                    if (args.length == 3 && args[1].equals("--")) {
+                        String fileName = args[2];
+                        Repository.checkout(fileName);
+                    } else if (args.length == 4 && args[2].equals("--")) {
+                        String commitId = args[1];
+                        String fileName = args[3];
+                        Repository.checkout(fileName, commitId);
+                    } else if (args.length == 2) {
+                        String branch = args[1];
+                        Repository.checkoutBranch(branch);
+                    } else {
+                        throw new GitletException("Incorrect operands.");
+                    }
+                    break;
+                case "find":
+                    validateRepo();
+                    validateNumArgs("find", args, 2);
+                    text = args[1];
+                    Repository.find(text);
+                    break;
+                case "reset":
+                    validateRepo();
+                    validateNumArgs("reset", args, 2);
+                    text = args[1];
+                    Repository.reset(text);
+                    break;
+                case "branch":
+                    validateRepo();
+                    validateNumArgs("branch", args, 2);
+                    text = args[1];
+                    Repository.branch(text);
+                    break;
+                case "rm-branch":
+                    validateRepo();
+                    validateNumArgs("rm-branch", args, 2);
+                    text = args[1];
+                    Repository.rmBranch(text);
+                    break;
+                case "rm":
+                    validateRepo();
+                    validateNumArgs("rm", args, 2);
+                    text = args[1];
+                    Repository.rm(text);
+                    break;
+                default:
+                    throw new GitletException("No command with that name exists.");
+            }
+        } catch (IOException e) {
+            System.err.println("An IO error occurred: " + e.getMessage());
         }
     }
 
